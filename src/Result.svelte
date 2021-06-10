fb8e2b29aa49c9901e01a1160d371a0c09809ff4
Federico Perez <federico.g.perez@despegar.com>
2021-06-10 14:31:55 -0300

    Changed result text

--- src/Result.svelte
+++ src/Result.svelte
@@ -5,6 +5,14 @@
     export let lvl;
 
     let up = hp - ((lvl - 13) * race.average + race.hpAt13);
+
+    let text;
+
+    if (up > 0) {
+        text = `Tu personaje tiene <b>${up}</b> de vida más de lo normal`;
+    } else if (up < 0) {
+        text = `Tu personaje esta bajo <b>${up}</b> del promedio`;
+    } else text = 'Tu personaje tiene la vida promedio acorde al nivel';
 </script>
 
 <div class="section">
@@ -12,7 +20,7 @@
     <p>Un <b>{clazz} {race.race}</b> con nivel <b>{lvl}</b> debería tener: 
     <b>{race.average * (lvl - 13) + race.hpAt13} de vida</b></p>
 
-    <p>Tu personaje tiene <b>{up}</b> más de lo normal</p>
+    <p>{@html text}</p>
 </div>
 </div>
 
