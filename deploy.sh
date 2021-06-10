f87e1f09e385ecac6b142f9c20c981597ddbe75b
Federico Perez <federico.g.perez@despegar.com>
2021-06-10 10:42:19 -0300

    Changed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 rm -R docs/
 cp -R public/ docs/
+
+sed 's/<title>Calculadora de Vida<\/title>/<base href="https:\/\/fedelway.github.io\/calculadora-de-vida\/">\n\t<title>Calculadora de Vida<\/title>/' docs/index.html > docs/index.html.tmp && mv docs/index.html.tmp docs/index.html
